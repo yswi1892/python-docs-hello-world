9f5eb7a5dc20fd9435f1b61c3489880423fc727f
sawai_y <yutaka.sawai@sicrystal.de>
2021-10-26 18:13:24 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.web import WebSiteManagementClient
+import datetime
+import time
+import psutil 
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    SUBSCRIPTION_ID = "e8ea57f1-23d1-4841-9864-74bf2461f953"
+    GROUP_NAME = "yukurash_rg_Linux_japaneast"
+    APP_SERVICE_PLAN = "yukurash_asp_Linux_japaneast_0"
+    
+    web_client = WebSiteManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id=SUBSCRIPTION_ID
+    )
+    # Get app service plan
+    app_service_plan = web_client.app_service_plans.get(
+        GROUP_NAME,
+        APP_SERVICE_PLAN
+    )
+    print("Get app service plan:\n{}".format(app_service_plan.sku))
+    
+    #Create app service plan
+    app_service_plan = web_client.app_service_plans.begin_create_or_update(
+        GROUP_NAME,
+        APP_SERVICE_PLAN,
+        {
+            "location": "japaneast",
+            "sku": {
+                "name": "P3v2",
+                "tier": "PremiumV2",
+                "size": "P3v2",
+                "family": "Pv2",
+                "capacity": "1"
+            }
+        }
+    ).result()
+    print("Create app service plan:\n{}".format(app_service_plan.sku))
+    
+    ###########
+
+    print("Machine Learning")
+
+    for i in range(6):
+        print({
+            'cpu_count':psutil.cpu_count(),
+            'cpu_count_logical':psutil.cpu_count(logical=False),
+            'cpu_percent':psutil.cpu_percent(interval=1, percpu=True)
+        })
+        
+        #sleep 10s
+        time.sleep(10)
+    
+    # Create app service plan
+    app_service_plan = web_client.app_service_plans.begin_create_or_update(
+        GROUP_NAME,
+        APP_SERVICE_PLAN,
+        {
+            "location": "japaneast",
+            "sku": {
+                "name": "P1v2",
+                "tier": "PremiumV2",
+                "size": "P1v2",
+                "family": "Pv2",
+                "capacity": "2"
+            }
+        }
+    ).result()
+    print("Create app service plan:\n{}".format(app_service_plan.sku))
+    
+    ##################
+    
+    dt_now = datetime.datetime.now()
+    print('time from app:', dt_now)
+
+    return "current sku: {}".format(app_service_plan.sku)
+    
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
